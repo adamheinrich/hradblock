72cd0ab379f7598ffa10f6a86ef538425ca57ac9
Adam Heinrich <adam@adamh.cz>
2016-11-01 20:32:13 +0100

    lidovky: Ochrana proti odstraneni #mainart, je-li shoda v .shp-style

--- src/lidovky.js
+++ src/lidovky.js
@@ -1,10 +1,14 @@
 var numRemoved = 0;
 
-numRemoved += wtdFilter(document.getElementsByClassName("mainart"));
+/* Nejdrive konkretni, pak obecne -- abychom napr. zabranili odstraneni celeho
+   #mainart, je-li hledany vyraz v ul.shp-style li */
+
+numRemoved += wtdFilter($("ul.shp-style li"));
+numRemoved += wtdFilter($(".arts-1 li"));
+numRemoved += wtdFilter($("#rubrika-nejctenejsi .art a"));
 numRemoved += wtdFilter($(".row .art"));
 numRemoved += wtdFilter($(".list-art .art"));
-numRemoved += wtdFilter($("#rubrika-nejctenejsi .art a"));
-numRemoved += wtdFilter($(".arts-1 li"));
+numRemoved += wtdFilter(document.getElementsByClassName("mainart"));
 
 if (isDebugMode) {
 	alert('Lidovky.cz: Removed ' + numRemoved + ' articles.');
